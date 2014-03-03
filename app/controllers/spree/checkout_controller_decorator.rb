260713fbde7155f6d8ab5356f57eec4d57ca5a61
mespina <mespina@acid.cl>
2014-03-03 15:06:08 -0300

    remove white-spaces into app/controllers/spree/checkout_controller_decorator.rb

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,9 +3,8 @@ module Spree
     def edit
       @payment = @order.payments.order(:id).last
 
-      
       redirect_to puntopagos_success_path(@payment.token) and return if @payment and @payment.token? and ['processing', 'completed'].include?(@payment.state)
-      
+
       redirect_to puntopagos_error_path(@payment.token) and return if @payment and @payment.token? and @payment.failed? and params[:state] != 'payment'
 
       if params[:state] == Spree::Gateway::Puntopagos::STATE and @order.state == Spree::Gateway::Puntopagos::STATE
@@ -18,7 +17,7 @@ module Spree
         provider = payment_method.provider.new
         response = provider.make_request(trx_id, amount, api_payment_method)
 
-        if response.success?          
+        if response.success?
           # TODO - ver si se puede reutilizar el token cuando este ya esta seteado
           @payment.update_attributes token: response.get_token
           redirect_to response.payment_process_url
