d0a77b4c127d8ac7c774df3c562d3ebbdd01b960
mespina <mespina@acid.cl>
2014-03-04 17:00:53 -0300

    fix routes for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Spree::Core::Engine.routes.draw do
   # The notification URL
-  post 'spree/puntopagos/confirmation/:token', to: 'puntopagos#confirmation', as: :puntopagos_confirmation
+  post '/spree/puntopagos/confirmation/:token', to: 'puntopagos#confirmation', as: :puntopagos_confirmation
 
   # The success URL
-  get 'spree/puntopagos/success/:token', to: 'puntopagos#success', as: :puntopagos_success
+  get '/spree/puntopagos/success/:token', to: 'puntopagos#success', as: :puntopagos_success
 
   # The failure URL
-  get 'spree/puntopagos/error/:token', to: 'puntopagos#error', as: :puntopagos_error
+  get '/spree/puntopagos/error/:token', to: 'puntopagos#error', as: :puntopagos_error
 end
--- lib/spree_puntopagos/version.rb
+++ lib/spree_puntopagos/version.rb
@@ -1,3 +1,3 @@
 module SpreePuntopagos
-  VERSION = '2.2.2'
+  VERSION = '2.2.3'
 end
