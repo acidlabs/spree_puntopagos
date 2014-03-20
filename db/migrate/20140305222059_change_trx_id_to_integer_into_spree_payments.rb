632bfe9661fa2d0cbaaec78c9cf7565b72ea8ccd
mespina <mespina@acid.cl>
2014-03-20 14:51:16 -0400

    fix migration for postgres db

--- db/migrate/20140305222059_change_trx_id_to_integer_into_spree_payments.rb
+++ db/migrate/20140305222059_change_trx_id_to_integer_into_spree_payments.rb
@@ -1,5 +1,5 @@
 class ChangeTrxIdToIntegerIntoSpreePayments < ActiveRecord::Migration
   def change
-    change_column :spree_payments, :trx_id, :integer
+    change_column :spree_payments, :trx_id, 'integer USING CAST(trx_id AS integer)'
   end
 end
