c99bc948f609bbc293da74f1a966e1dd5727c428
mespina <mespina@acid.cl>
2014-03-26 11:16:50 -0400

    fix controller #edit override

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,41 +1,44 @@
 module Spree
   CheckoutController.class_eval do
-    def edit
-      @payment = @order.payments.order(:id).last
+    before_filter :check_puntopagos, only: :edit
 
-      redirect_to puntopagos_success_path(@payment.token) and return if @payment and @payment.token? and ['processing', 'completed'].include?(@payment.state)
+    private
+      def check_puntopagos
+        @payment = @order.payments.order(:id).last
 
-      redirect_to puntopagos_error_path(@payment.token) and return if @payment and @payment.token? and @payment.failed? and params[:state] != 'payment'
+        redirect_to puntopagos_success_path(@payment.token) and return if @payment and @payment.token? and ['processing', 'completed'].include?(@payment.state)
 
-      if params[:state] == Spree::Gateway::Puntopagos.STATE and @order.state == Spree::Gateway::Puntopagos.STATE
-        payment_method     = @order.payment_method
+        redirect_to puntopagos_error_path(@payment.token) and return if @payment and @payment.token? and @payment.failed? and params[:state] != 'payment'
 
-        trx_id             = @payment.trx_id.to_s
-        api_payment_method = payment_method.has_preference?(:api_payment_method) ? payment_method.preferred_api_payment_method : nil
-        amount             = @order.puntopagos_amount
+        if params[:state] == Spree::Gateway::Puntopagos.STATE and @order.state == Spree::Gateway::Puntopagos.STATE
+          payment_method     = @order.payment_method
 
-        provider = payment_method.provider.new
-        response = provider.make_request(trx_id, amount, api_payment_method)
+          trx_id             = @payment.trx_id.to_s
+          api_payment_method = payment_method.has_preference?(:api_payment_method) ? payment_method.preferred_api_payment_method : nil
+          amount             = @order.puntopagos_amount
 
-        if response.success?
-          # TODO - ver si se puede reutilizar el token cuando este ya esta seteado
-          @payment.update_attributes token: response.get_token
+          provider = payment_method.provider.new
+          response = provider.make_request(trx_id, amount, api_payment_method)
 
-          # To clean the Cart
-          session[:order_id] = nil
-          @current_order     = nil
+          if response.success?
+            # TODO - ver si se puede reutilizar el token cuando este ya esta seteado
+            @payment.update_attributes token: response.get_token
 
-          redirect_to response.payment_process_url and return
-        else
-          @error = response.get_error
-          @payment.update_attributes puntopagos_params: {error: @error}
+            # To clean the Cart
+            session[:order_id] = nil
+            @current_order     = nil
 
-          unless ['processing', 'failed'].include?(@payment.state)
-            @payment.started_processing!
-            @payment.failure!
+            redirect_to response.payment_process_url and return
+          else
+            @error = response.get_error
+            @payment.update_attributes puntopagos_params: {error: @error}
+
+            unless ['processing', 'failed'].include?(@payment.state)
+              @payment.started_processing!
+              @payment.failure!
+            end
           end
         end
       end
-    end
   end
 end
--- lib/spree_puntopagos/version.rb
+++ lib/spree_puntopagos/version.rb
@@ -1,3 +1,3 @@
 module SpreePuntopagos
-  VERSION = '3.7.5'
+  VERSION = '3.7.6'
 end
