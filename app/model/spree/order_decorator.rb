074d34d1f194e7192ef537f9abe9d272550dab40
mespina <mespina@acid.cl>
2014-03-27 15:18:57 -0400

    fix para checkout_flow en pago con errores

--- app/model/spree/order_decorator.rb
+++ app/model/spree/order_decorator.rb
@@ -9,9 +9,9 @@ module Spree
     checkout_flow do
       go_to_state :address
       go_to_state :delivery
-      go_to_state :payment,    if: ->(order) { order.payment_required?              }
-      go_to_state :puntopagos, if: ->(order) { order.has_puntopagos_payment_method? }
-      go_to_state :confirm,    if: ->(order) { order.confirmation_required?         }
+      go_to_state :payment,    if: ->(order) { order.payment_required? }
+      go_to_state :puntopagos, if: ->(order) { order.has_puntopagos_payment_method? or order.state == Spree::Gateway::Puntopagos.STATE }
+      go_to_state :confirm,    if: ->(order) { order.confirmation_required? }
       go_to_state :complete
       remove_transition from: :delivery, to: :confirm
     end
